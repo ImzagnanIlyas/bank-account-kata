976eeff427861fec6c23c4af652fe8c93081806a
ILYAS IMZAGNAN <imzagnan.ii@gmail.com>
2022-07-30 21:36:07 +0100

    create a local database

---
+++ src/main/java/org/ensias/miola/utils/LocalDatabase.java
@@ -0,0 +1,10 @@
+package org.ensias.miola.utils;
+
+import org.ensias.miola.entities.Account;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LocalDatabase {
+    public static List<Account> accounts = new ArrayList<>(); // for simulation
+}
